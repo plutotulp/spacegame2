b7f6589b7ce638a655c2b733be608186500ee2c9
Erlend Pedersen <erlendpe@online.no>
2023-02-18 19:18:01 +0100

    Init

---
+++ scenes/Main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Player.hide()
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func new_game():
+	$Player.start($StartingPosition.position)
+	$Player.show()
